47b6d97d83efe3c6617dbb4bf561c0845b745c7a
Alex Bicknell <60597067+kuuzon@users.noreply.github.com>
2023-07-31 15:31:36 +1000

    css reset applied

--- client/src/styles/resets.css.ts
+++ client/src/styles/resets.css.ts
@@ -10,22 +10,32 @@ globalStyle("body, h1, h2, h3, h4, p, figure, blockquote, dl, dd", {
   margin: 0
 })
 
-// 
+// Set core root defaults
+globalStyle("html:focus-within", {
+  scrollBehavior: "smooth"
+})
 
+// Set core body defaults
 globalStyle("body", {
-  margin: 0,
-  padding: 0,
-  border: 0,
   minHeight: "100vh",
+  textRendering: "optimizeSpeed",
+  lineHeight: 1.5
   // ...
 });
 
+// Set core anchor settings
 globalStyle("a", {
   textDecoration: "none",
   color: "inherit",
 });
 
+// Make images easier to work with
 globalStyle("img, picture", {
   maxWidth: "100%",
   display: "block"
 });
+
+// Inherit fonts for inputs and buttons
+globalStyle("input, button, textarea, select", {
+  font: "inherit"
+})
