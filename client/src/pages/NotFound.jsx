fb4c0a94c46dae121a09dc96053385287bc1c859
Alex Bicknell <60597067+kuuzon@users.noreply.github.com>
2023-08-08 19:02:25 +1000

    404 page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
+import * as styles from './NotFound.css'
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className={styles.notFoundBox}>
+      <div className={styles.twinBox}>
+        <h1>404</h1>
+        <Link to="/">This page could not be found</Link>
+      </div>
+    </div>
   )
 }
 
