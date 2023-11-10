2d8d3180f84791932818f679757fd7ff9e237973
Alex Bicknell <60597067+kuuzon@users.noreply.github.com>
2023-11-10 11:31:30 +1100

    update to vite.config for dynamic constants

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    vanillaExtractPlugin() // https://vanilla-extract.style/documentation/integrations/vite/
-  ],
-  server: {
-    // https://vitejs.dev/config/server-options.html#server-port
-    port: 3000,
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '')
 
-    // https://vitejs.dev/config/server-options.html#server-proxy
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [
+      react(),
+      vanillaExtractPlugin() // https://vanilla-extract.style/documentation/integrations/vite/
+    ],
+    server: {
+      // https://vitejs.dev/config/server-options.html#server-port
+      port: env.VITE_PORT,
+  
+      // https://vitejs.dev/config/server-options.html#server-proxy
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
